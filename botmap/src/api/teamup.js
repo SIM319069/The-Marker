4946458a28a51aa3f155c7dbd911d809e5cb7150
winnieBinnie <ritthanupahp_s@cmu.ac.th>
2024-07-15 18:32:16 +0700

    Added API but still invalid date

---
+++ botmap/src/api/teamup.js
@@ -0,0 +1,22 @@
+
+import axios from 'axios';
+
+const API_KEY = '8e4d82f938d73b8ee730140e9b48f9c5bf8fcfe874eb5058f0fe30a0b8fdd1fe';
+const BASE_URL = 'https://teamup.com/ksg7y4nwkfp7q6xyio';
+
+const apiClient = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'Teamup-Token': API_KEY,
+    }
+});
+
+export const fetchData = async () => {
+    try {
+        const response = await apiClient.get('/');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching data', error);
+        throw error;
+    }
+};
