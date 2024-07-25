e0fcbae9e8ebe71a34981c38ca89878685bb0a86
Nat <nat_kempet@cmu.ac.th>
2024-07-25 09:40:24 +0700

    (API) Can pull range of days

--- botmap/src/api/teamup.js
+++ botmap/src/api/teamup.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
 
 const API_KEY = '8e4d82f938d73b8ee730140e9b48f9c5bf8fcfe874eb5058f0fe30a0b8fdd1fe';
-const BASE_URL = 'https://api.teamup.com/ksg7y4nwkfp7q6xyio';
-
+const options = {
+    method: 'GET',
+    url: 'https://api.teamup.com/ksg7y4nwkfp7q6xyio/events',
+    params: {startDate: '2024-07-21', endDate: '2024-07-27'},
+    headers: {Accept: 'application/json', 'Teamup-Token': '123', Authorization: 'Bearer 123'}
+  };
+  
+  try {
+    const { data } = await axios.request(options);
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+  
 const apiClient = axios.create({
     baseURL: BASE_URL,
     headers: {
