10c8e2d4d82a2fd82f6aadb2be60d8619fa7902a
PeerawatSi <peerawat_si@cmu.ac.th>
2024-09-05 08:49:17 +0700

    first commit by peace

--- botmap/src/components/Summary.js
+++ botmap/src/components/Summary.js
@@ -6,16 +6,17 @@ import useFetchEvents from "../api/teamup";
 import moment from "moment";
 
 function Summary() {
-  const [selectedCategory, setSelectedCategory] = useState("majorChart");
+  const [selectedCategory, setSelectedCategory] = useState("dayChart");
   const [mode, setMode] = useState("today");
   const events = useFetchEvents();
   const [filters, setFilters] = useState({
-    cpe: false,
-    mcpe: false,
-    isne: false,
-    RoomReservation: false,
-    Xternalmcpe: false,
-    XternalUndergrad: false,
+    monday: false,
+    tuesday: false,
+    wednesday: false,
+    thursday: false,
+    friday: false,
+    saturday: false,
+    sunday: false, 
   });
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -53,14 +54,19 @@ function Summary() {
 
   const filterEvents = (event) => {
     let include = true;
-    if (filters.cpe && !event.title.includes("cpe")) include = false;
-    if (filters.mcpe && !event.title.includes("mcpe")) include = false;
-    if (filters.isne && !event.title.includes("isne")) include = false;
-    if (filters.RoomReservation && !event.title.includes("Room reservation"))
+    if (filters.monday && !event.title.toLowerCase().includes("monday"))
       include = false;
-    if (filters.Xternalmcpe && !event.title.includes("xternal mcpe"))
+    if (filters.tuesday && !event.title.toLowerCase().includes("tuesday"))
       include = false;
-    if (filters.XternalUndergrad && !event.title.includes("xternal Undergrad"))
+    if (filters.wednesday && !event.title.toLowerCase().includes("wednesday"))
+      include = false;
+    if (filters.thursday && !event.title.toLowerCase().includes("thursday"))
+      include = false;
+    if (filters.friday && !event.title.toLowerCase().includes("friday"))
+      include = false;
+    if (filters.saturday && !event.title.toLowerCase().includes("saturday"))
+      include = false;
+    if (filters.sunday && !event.title.toLowerCase().includes("sunday"))
       include = false;
     return include;
   };
@@ -73,23 +79,31 @@ function Summary() {
   const roomUsage = filteredEvents.reduce((acc, event) => {
     const room = event.location || "No location specified";
     const title = event.title.toLowerCase();
-    const major = title.includes("cpe")
-      ? "CPE"
-      : title.includes("mcpe")
-      ? "MCPE"
-      : title.includes("isne")
-      ? "ISNE"
+    const day = title.includes("monday")
+      ? "monday"
+      : title.includes("tuesday")
+      ? "tuesday"
+      : title.includes("wednesday")
+      ? "wednesday"
+      : title.includes("thursday")
+      ? "thursday"
+      : title.includes("friday")
+      ? "friday"
+      : title.includes("saturay")
+      ? "saturday"
+      : title.includes("sunday")
+      ? "sunday"
       : "OTHER";
 
     if (room !== "No location specified" && !room.includes("xxx")) {
       if (!acc[room]) {
         acc[room] = {
           total: 0,
-          majors: { CPE: 0, MCPE: 0, ISNE: 0, OTHER: 0 },
+          days: { monday: 0, tuesday: 0, wednesday: 0, thursday: 0, friday: 0, saturday: 0, sunday: 0 },
         };
       }
       acc[room].total++;
-      acc[room].majors[major]++;
+      acc[room].days[day]++;
     }
     return acc;
   }, {});
@@ -111,52 +125,59 @@ function Summary() {
     ],
   };
 
-  const majorUsage = Object.values(roomUsage).reduce(
+  const dayUsage = Object.values(roomUsage).reduce(
     (acc, usage) => {
-      Object.keys(usage.majors).forEach((major) => {
-        if (!acc[major]) {
-          acc[major] = 0;
+      Object.keys(usage.days).forEach((day) => {
+        if (!acc[day]) {
+          acc[day] = 0;
         }
-        acc[major] += usage.majors[major];
+        acc[day] += usage.days[day];
       });
       return acc;
     },
-    { CPE: 0, MCPE: 0, ISNE: 0, OTHER: 0 }
+    { monday: 0, tuesday: 0, wednesday: 0, thursday: 0, friday: 0, saturday: 0, sunday: 0, OTHER: 0 }
   );
 
   const doughnutData = {
-    labels: ["ISNE", "CPE", "MCPE", "OTHER"],
+    labels: ["monday",  "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
     datasets: [
       {
-        label: "Major Usage",
+        label: "day Usage",
         data: [
-          majorUsage.ISNE,
-          majorUsage.CPE,
-          majorUsage.MCPE,
-          majorUsage.OTHER,
+          dayUsage.monday,
+          dayUsage.tuesday,
+          dayUsage.wednesday,
+          dayUsage.thursday,
+          dayUsage.friday,
+          dayUsage.saturday,
+          dayUsage.sunday,
         ],
-        backgroundColor: ["#36a2eb", "#ff9f40", "#4bc0c0", "#ff6384"],
+        backgroundColor: ["#deeb34", "#de008d", "#34eb71", "#34eb71", "#349ceb", "#df34eb", "#eb4934"],
       },
     ],
   };
-
-  const cpeCount = filteredEvents.filter((event) =>
-    event.title.toLowerCase().includes("cpe")
-  ).length;
-  const mcpeCount = filteredEvents.filter((event) =>
-    event.title.toLowerCase().includes("mcpe")
-  ).length;
-  const isneCount = filteredEvents.filter((event) =>
-    event.title.toLowerCase().includes("isne")
-  ).length;
-
-  const majorchartData = {
-    labels: ["ISNE", "CPE", "MCPE"],
+  const mondayCount = filteredEvents.filter((event) => 
+    event.title.toLowerCase().includes("monday")).length;
+  const tuesdayCount = filteredEvents.filter((event) => 
+    event.title.toLowerCase().includes("tuesday")).length;
+  const wednesdayCount = filteredEvents.filter((event) => 
+    event.title.toLowerCase().includes("wednesday")).length;
+  const thursdayCount = filteredEvents.filter((event) => 
+    event.title.toLowerCase().includes("thursday")).length;
+  const fridayCount = filteredEvents.filter((event) => 
+    event.title.toLowerCase().includes("friday")).length;
+  const saturdayCount = filteredEvents.filter((event) => 
+    event.title.toLowerCase().includes("saturday")).length;
+  const sundayCount = filteredEvents.filter((event) => 
+    event.title.toLowerCase().includes("sunday")).length;
+  
+  const daychartData = {
+    labels: ["monday",  "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
     datasets: [
       {
         label: "Event Count",
-        data: [isneCount, cpeCount, mcpeCount],
-        backgroundColor: ["#36a2eb", "#ff9f40", "#4bc0c0"],
+        data: [mondayCount, tuesdayCount, wednesdayCount, thursdayCount,fridayCount,saturdayCount,sundayCount ],
+        backgroundColor: ["#deeb34", "#de008d", "#34eb71", "#34eb71", "#349ceb", "#df34eb", "#eb4934"],
       },
     ],
   };
@@ -255,9 +276,9 @@ function Summary() {
         </div>
         <nav className="mb-5">
           <button
-            onClick={() => setSelectedCategory("majorChart")}
+            onClick={() => setSelectedCategory("dayChart")}
             className={`mr-3 px-3 py-2 ${
-              selectedCategory === "majorChart"
+              selectedCategory === "dayChart"
                 ? "bg-blue-500 text-white"
                 : "bg-gray-200"
             }`}
@@ -282,7 +303,7 @@ function Summary() {
                 : "bg-gray-200"
             }`}
           >
-            Major Usage
+            Day Usage
           </button>
           <button
             onClick={() => setSelectedCategory("statistic")}
@@ -296,9 +317,9 @@ function Summary() {
           </button>
         </nav>
         <div className="grid grid-cols-1 gap-5 p-5">
-          {selectedCategory === "majorChart" && (
+          {selectedCategory === "dayChart" && (
             <div className="w-full h-[400px]">
-              <Bar data={majorchartData} />
+              <Bar data={daychartData} />
             </div>
           )}
           {selectedCategory === "roomChart" && (
